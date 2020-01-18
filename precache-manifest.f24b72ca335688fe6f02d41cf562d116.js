a4e84cc602ec8d4b324b12c3b0ee4ea134643a75
jmbodnar <jmbodnar@protonmail.com>
2020-01-18 09:16:29 -0500

    Updates

---
+++ precache-manifest.f24b72ca335688fe6f02d41cf562d116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d350d1524fa7413f22587f9f77478b9",
+    "url": "/router-practice/index.html"
+  },
+  {
+    "revision": "83ab15099308e96cee3e",
+    "url": "/router-practice/static/css/main.fc20fa56.chunk.css"
+  },
+  {
+    "revision": "416f19fa80a65a68792b",
+    "url": "/router-practice/static/js/2.79127d0a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/router-practice/static/js/2.79127d0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83ab15099308e96cee3e",
+    "url": "/router-practice/static/js/main.46c1e1cc.chunk.js"
+  },
+  {
+    "revision": "5d239093b6e27533f461",
+    "url": "/router-practice/static/js/runtime-main.811ab839.js"
+  }
+]);
