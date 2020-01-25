986a030afbb303198b55ca47680506e1cf3bf862
Jon Bodnar <jon.bodnar@gmail.com>
2020-01-25 10:20:21 -0500

    update main jumbotron: add descriptive text

--- src/components/main-header.jsx
+++ src/components/main-header.jsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="jumbotron mb-0">
-      <h1 className="text-center">
+    <header className="jumbotron text-center mb-0">
+      <h1 className="display-4">
         <Link to="/">‘Tis the Seasoning</Link>
       </h1>
+      <p className="container my-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+        voluptatum quidem harum.
+      </p>
     </header>
   );
 };
